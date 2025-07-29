16e6e935aac4d35081077442a88b2d8af6f1e838
mohamamd.abedy <smh.abedy@tapsell.ir>
2025-07-29 22:23:20 +0330

    v1.0.3

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='PyApso',
-    version='1.0.0',
+    name='pyapso',
+    version='1.0.3',  
     description='Adaptive Particle Swarm Optimization for continuous, multi-objective, and multidimensional problems',
-    author='SMH Abedy',
-    author_email='m.h.o.abedu@gmail.com',
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/smohamadabedy/pyapso',  # replace with your GitHub
+    project_urls={
+        "Documentation": "https://github.com/smohamadabedy/pyapso#readme",
+        "Source": "https://github.com/smohamadabedy/pyapso",
+        "Issues": "https://github.com/smohamadabedy/pyapso/issues",
+    },
+    license='MIT',
+    keywords='optimization particle-swarm metaheuristic multiobjective',
     packages=find_packages(),
     install_requires=[
         'numpy',
         'tqdm',
         'matplotlib'
     ],
-    python_requires='>=3.6',
+    include_package_data=True,
 )
