9a4db9cbd7ad8b269213e22494e364e5c0980e5a
mohamamd.abedy <smh.abedy@tapsell.ir>
2025-07-29 23:09:41 +0330

    fin

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyapso',
-    version='1.0.3',  
+    version='1.0.5',  
     description='Adaptive Particle Swarm Optimization for continuous, multi-objective, and multidimensional problems',
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
